633897d8bf24efc0900d45eb4514d27a8e164553
DiViNeSiC <divine.beatbox@gmail.com>
2020-09-17 13:58:39 +0430

    Fixed Cors Origin

--- server.js
+++ server.js
@@ -49,8 +49,9 @@ db.once('open', () =>
 )
 
 //Use Middlewares
-app.options('*', access)
-app.use(cors())
+app.use(cors({ 
+    credentials: true
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
